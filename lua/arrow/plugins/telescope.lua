a49d5d7c775d420b9282f2b9ea39d7dc0b5c63f9
Mark Arrow <mark.arrow@movu-robotics.com>
2024-08-05 08:28:00 +0200

    Added additional plugins and fixed use of deprecated function
    Trouble and Telescope integration made use of a deprecated function.
    Replaced this function with the newer recommended function

--- lua/arrow/plugins/telescope.lua
+++ lua/arrow/plugins/telescope.lua
@@ -14,7 +14,7 @@ return {
 
     local trouble = require("trouble")
     local trouble_telescope = require("trouble.providers.telescope")
-
+    local open_with_trouble = require("trouble.sources.telescope").open
     -- or create your custom action
     local custom_actions = transform_mod({
       open_trouble_qflist = function(prompt_bufnr)
@@ -30,7 +30,7 @@ return {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
+            ["<C-t>"] = open_with_trouble,
           },
         },
       },
