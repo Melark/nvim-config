7bb99dc481fc5d6b82ebcc083e37e4d8f8354b30
Mark Arrow <marka@Marks-MacBook-Pro.local>
2024-08-12 21:40:45 +0200

    Hide quicksettings for now

--- lua/josean/plugins/gruvbox.lua
+++ lua/josean/plugins/gruvbox.lua
@@ -2,14 +2,14 @@ return {
   "ellisonleao/gruvbox.nvim",
   priority = 1000,
   config = function()
-    require("quick-settings")
+    -- require("quick-settings")
+    -- vim.cmd.colorscheme("gruvbox")
+    --
+    -- if vim.g.gs_theme == "light" then
+    --   vim.o.background = "light"
+    -- else
+    vim.o.background = "dark"
     vim.cmd.colorscheme("gruvbox")
-
-    if vim.g.gs_theme == "light" then
-      vim.o.background = "light"
-    else
-      vim.o.background = "dark"
-      vim.cmd.colorscheme("gruvbox")
-    end
+    -- end
   end,
 }
