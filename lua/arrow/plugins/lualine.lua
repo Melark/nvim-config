423020fe0bf02764c582c33bb2476cd07b803670
Mark Arrow <marka@Marks-MacBook-Pro.local>
2024-10-14 07:03:15 +0200

    Added more themes

--- lua/arrow/plugins/lualine.lua
+++ lua/arrow/plugins/lualine.lua
@@ -19,7 +19,7 @@ return {
     -- configure lualine with modified theme
     lualine.setup({
       options = {
-        theme = "molokai",
+        theme = "solarized_light",
       },
       sections = {
         lualine_x = {
