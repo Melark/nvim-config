23f4e57c41c30f07da43dfcb69e5aa8093614cbe
Mark Arrow <mark.arrow@movu-robotics.com>
2025-06-03 21:55:27 +0200

    wip

--- lua/arrow/plugins/theme.lua
+++ lua/arrow/plugins/theme.lua
@@ -1,8 +1,21 @@
--- lua/plugins/rose-pine.lua
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine")
-	end
+  -- {
+  -- 	"rose-pine/neovim",
+  -- 	name = "rose-pine",
+  -- 	config = function()
+  -- 		vim.cmd("colorscheme rose-pine")
+  -- 	end
+  -- },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_transparent_background = true
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
 }
